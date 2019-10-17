abaf5fe7eac34d2d8d339d0b5aa3bd6814ec238d
Dominic Flavio Tjiptono <dtji0001@student.monash.edu>
2019-10-17 21:46:22 +1300

    Second commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='New Password Generator',
+    version='1',
+    packages=['New Password Generator'],
+    url='',
+    license='',
+    author='Dominic Tjiptono',
+    author_email='',
+    description=''
+)
